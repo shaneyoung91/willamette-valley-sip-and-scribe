773c442fc79a45f6a7ec9d595bf121a63776072d
shaneyoung91 <shaneadam.young@gmail.com>
2023-08-24 16:34:45 -0700

    Create  6 Wineries to seed

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -35,9 +35,8 @@ export default function Review({ user, reviews, setReviews, winery }) {
     async function handleUpdate(editReview) {
         try {
             await reviewsAPI.updateReview(editReview._id, editReview);
-            const editReviews = reviews.map(review =>
-                review._id === editReview._id ? editReview : review
-            );
+            const editReviews = reviews.map(review => review._id === 
+                editReview._id ? editReview : review);
             setReviews(editReviews);
             setEditReviewId(null);
         } catch (error) {
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav} from 'react-bootstrap';
 import * as usersService from '../../utilities/users-service';
 import './SideBar.css';
 
